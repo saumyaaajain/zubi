ff97dd51c64592f8341299e9268f6d946875783c
saumya <jsaumya988@gmail.com>
2020-03-29 17:22:27 +0530

    Updates

---
+++ precache-manifest.174e6fb22598ad166944b4abb31c7db8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94263ad4113029369ac514134e0aab23",
+    "url": "/zubi/index.html"
+  },
+  {
+    "revision": "dfe5ca6112a22e11c76e",
+    "url": "/zubi/static/css/2.926e15a4.chunk.css"
+  },
+  {
+    "revision": "bb71c59868ba1598e186",
+    "url": "/zubi/static/css/main.b9b34d58.chunk.css"
+  },
+  {
+    "revision": "dfe5ca6112a22e11c76e",
+    "url": "/zubi/static/js/2.1294ffb5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zubi/static/js/2.1294ffb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb71c59868ba1598e186",
+    "url": "/zubi/static/js/main.91fabcfd.chunk.js"
+  },
+  {
+    "revision": "2ff8647fd391e0b37a26",
+    "url": "/zubi/static/js/runtime-main.344662fc.js"
+  },
+  {
+    "revision": "c927cdbcc44872dc02a0af783ac79dcc",
+    "url": "/zubi/static/media/UNKNOWN-2.c927cdbc.png"
+  },
+  {
+    "revision": "e0d2b7486b532288a980d0cd5d535340",
+    "url": "/zubi/static/media/chat-icon.e0d2b748.svg"
+  },
+  {
+    "revision": "7718b3e3ff59dac728c55e725a3b0523",
+    "url": "/zubi/static/media/logo-no-bg.7718b3e3.svg"
+  },
+  {
+    "revision": "0d062b33a8b60acb2a497afe63666b16",
+    "url": "/zubi/static/media/notification.0d062b33.mp3"
+  }
+]);
